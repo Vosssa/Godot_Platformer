b03b04e18325679424bb952b4bc6a3f537a46e2a
Vossa <veit.voss2001@gmail.com>
2024-02-13 23:08:21 +0100

    ich hasse mein leben das is alles viel zu viel

--- Player/player.gd
+++ Player/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+var SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
+var direction = Input.get_axis("move_backward", "move_forward")
+var canDash = true
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim = get_node("AnimationPlayer")
@@ -11,17 +13,19 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("Jump")
 	if velocity.y > 0:
 			anim.play("Fall")
+	if velocity.y == 0:
+		anim.play("Run")
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("move_backward", "move_forward")
 	if direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
 	elif direction == 1:
@@ -29,16 +33,22 @@ func _physics_process(delta):
 		
 	if direction:
 		velocity.x = direction * SPEED
-		if velocity.y == 0:
-			anim.play("Run")
+		
 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if velocity.y == 0:
 			anim.play("Idle")
-	
+
+	if Input.is_action_just_pressed("dash"):
+		velocity.x = direction * SPEED * 20
+		canDash = true
+		if canDash == true:
+			anim.play("Dash")
 	move_and_slide()
 
+	
+	
 	# if Game.playerHP <= 0:
 		# queue_free()
 		# get_tree().change_scene_to_file("res://main.tscn")
