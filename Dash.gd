2089a7bd16fa8c4974ecbc511ff8a24bd7808ddb
Vossa <veit.voss2001@gmail.com>
2024-02-14 19:10:16 +0100

    dash

---
+++ Dash.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+@onready var timer = $dashtimer
+
+func start_dash(duration):
+	timer.wait_time = duration
+	timer.start()
+	
+func is_dashing():
+	return !timer.is_stopped()
--- Player/player.gd
+++ Player/player.gd
@@ -1,18 +1,22 @@
 extends CharacterBody2D
 
-var SPEED = 200.0
+const NORMAL_SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+const dashspeed = 2500
+const dashlength = 1
 
-var direction = Input.get_axis("move_backward", "move_forward")
-var canDash = true
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim = get_node("AnimationPlayer")
+@onready var dash = $Dash
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	
+	
+	
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -25,30 +29,23 @@ func _physics_process(delta):
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_axis("move_backward", "move_forward")
+	var direction = Input.get_axis("move_backward", "move_forward")
 	if direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
 	elif direction == 1:
 		get_node("AnimatedSprite2D").flip_h = false
 		
 	if direction:
-		velocity.x = direction * SPEED
-		
-
+		velocity.x = direction * NORMAL_SPEED
+		if Input.is_action_just_pressed("dash"):
+			dash.start_dash(dashlength)
+			velocity.x = direction * dashspeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, NORMAL_SPEED)
 		if velocity.y == 0:
 			anim.play("Idle")
-
-	if Input.is_action_just_pressed("dash"):
-		velocity.x = direction * SPEED * 20
-		canDash = true
-		if canDash == true:
-			anim.play("Dash")
+	
 	move_and_slide()
-
-	
-	
 	# if Game.playerHP <= 0:
 		# queue_free()
 		# get_tree().change_scene_to_file("res://main.tscn")
